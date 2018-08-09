ececdd289bf2e0e29d50cdcc34f2febe0bc4758d
Chris Crow and Victoria Oh <victoria@>
2018-08-09 10:36:37 -0700

    add functionality. Depending on height, background will highlight to show the rides youre able to ride.

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,18 @@
 $(document).ready(function(){
 
-  
+  height = parseInt(prompt("How tall are you in feet rounded down? (we're looking for a single number here. Don't get fancy)"));
+
+  if (height >= 5) {
+    $(".atLeast5").addClass("highlight");
+    $(".atLeast4").addClass("highlight");
+    $(".all").addClass("highlight");
+  } else if (height < 5 && height >= 4) {
+    $(".atLeast4").addClass("highlight");
+    $(".all").addClass("highlight");
+  } else if (height < 4) {
+    $(".all").addClass("highlight");
+    $(".kids").addClass("highlight");
+  }
+
+
 });
